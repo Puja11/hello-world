d7373d49d60009214c7e69ebb53beeda13943232
Puja11 <puja11791@gmail.com>
2019-03-04 01:27:00 +0530

    Updates

---
+++ precache-manifest.11e2109867a611a94f8a506d11ff4b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-world/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d4505419795ec5c088c1",
+    "url": "/hello-world/static/js/runtime~main.d4505419.js"
+  },
+  {
+    "revision": "cb4637f71032c4e5868b",
+    "url": "/hello-world/static/js/main.cb4637f7.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/hello-world/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "cb4637f71032c4e5868b",
+    "url": "/hello-world/static/css/main.796a8988.chunk.css"
+  },
+  {
+    "revision": "66c887cc16d57941ad2ddd03e78de212",
+    "url": "/hello-world/index.html"
+  }
+];
